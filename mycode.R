4318b25b87d34fadd30e903c4d15073e5a7dc64f
upulcooray <upulcooray@gmail.com>
2020-06-09 22:47:45 +0900

    added example functions

--- mycode.R
+++ mycode.R
@@ -7,3 +7,24 @@ myfunction <- function() {
 second <- function(x) {
   x + rnorm(length(x))
 }
+
+above10 <- function(x){
+  filter <- x>10
+  x[filter]
+}
+
+above <- function(x,n=10){
+  filter <- x>n
+  x[filter]
+}
+
+columnlsmean <- function(y, removeNA = TRUE){
+  nc <- ncol(y)   #get number of columns 
+  means <- numeric(nc) # initialize an empty numeric vector to store means
+  for (i in 1:nc) {
+    means[i]<- mean(y[,i], na.rm = removeNA)
+    
+  }
+  means
+}
+
